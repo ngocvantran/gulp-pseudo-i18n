1bd98c6bdb1e2684800fc19fc0a5e70e6be61a97
Tran Ngoc Van <van.tran@easesolutions.com>
2014-10-03 18:24:19 +0800

    Re-implement relevant logic from node-pseudo-l10n and add support for exclusion.

--- index.js
+++ index.js
@@ -1,47 +1,60 @@
 'use strict';
 
-var pseudo = require('node-pseudo-l10n');
+var _ = require('lodash');
+var path = require('path');
 var gutil = require('gulp-util');
 var through = require('through2');
-var path = require('path');
-var _ = require('lodash');
+var gettext = require('gettext-parser');
+var pseudoTranslator = require('./lib/pseudo');
 
 var pluginName = require('./package.json').name;
 
 module.exports = function (options) {
-  options = _.extend({
-    potFile: true,
-    headerLanguage: 'qps-ploc'
-  }, options);
-
-  var emitStreamingError = function () {
-      this.emit('error', new gutil.PluginError(pluginName, 'Streaming not supported'));
-  }.bind(this);
+  options = _.extend({}, options, {
+    language: 'qps-ploc'
+  });
 
   return through.obj(function (file, enc, cb) {
-    
     if (file.isNull()) {
       this.push(file);
       return cb();
     }
 
     if (file.isStream()) {
-      emitStreamingError();
+      this.emit('error', new gutil.PluginError(pluginName, 'Streaming not supported'));
       return cb();
     }
-
-    var _this = this;
-	options.filePath = file.path;
     
-    pseudo(options, function(err, poFileBuffer) {
-      file.contents = poFileBuffer;
-      
-      var extension = path.extname(file.path);
-      var extIndex = file.path.lastIndexOf(extension);
-      file.path = file.path.substring(0, extIndex) + '.qps-ploc.po';
-      
-      _this.push(file);
-      cb(err);
+    // Parse PO/POT file
+    var parsed = gettext.po.parse(file.contents);
+    parsed.headers['language'] = options.language;
+    
+    // Translate
+    var translations = parsed.translations;
+    var pseudo = pseudoTranslator(options.charMap, options.excludes);
+    
+    Object.keys(translations).forEach(function (catalog) {
+      Object.keys(translations[catalog]).forEach(function (key) {
+        if (key.length === 0) return;
+
+        var strObj = translations[catalog][key];
+        strObj.msgstr[0] = pseudo(strObj.msgid);
+
+        if (strObj.msgid_plural)
+          strObj.msgstr[1] = pseudo(strObj.msgid_plural);
+      });
     });
+
+    file.contents = gettext.po.compile(parsed);
+    
+    // Rename abc.po or abc.pot to abc.[language].po
+    var filePath = file.path;
+    var dirname = path.dirname(filePath);
+    var extension = path.extname(filePath);
+    var basename = path.basename(filePath, extension);
+    file.path = path.join(dirname, basename + '.' + options.language + '.po');
+    
+    this.push(file);
+    cb();
   });
 }; 
